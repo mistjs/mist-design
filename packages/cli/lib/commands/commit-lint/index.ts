673ec876b65b3e0b31e60bbf378263cf33e8220b
aibayanyu <aibayanyu@qq.com>
2022-02-12 15:52:09 +0800

    fix: 增加husky验证工具

--- packages/cli/lib/commands/commit-lint/index.ts
+++ packages/cli/lib/commands/commit-lint/index.ts
@@ -1,25 +1,35 @@
 import { readFileSync } from "fs";
 import { consola } from "../../common/logger";
+import { isMonorepo, isMonorepoProject } from "../../common";
 
 const commitRE =
   /^(revert: )?(fix|feat|docs|perf|test|types|style|build|chore|release|refactor|breaking change)(\(.+\))?: .{1,50}/;
 const mergeRE = /Merge /;
 
-export function commitLint(gitParams: string) {
+const monorepoExample = `
+例子如下: 
+
+- fix(cli): 子项目cli的changelog
+- feat(ui): 子项目UI库的changelog
+- fix: 全局的changelog
+`;
+const baseExample = `
+例子如下: 
+
+- fix: 这是提交的项目信息
+- feat: 子项目UI库的changelog
+`;
+export async function commitLint(gitParams: string) {
+  await isMonorepoProject();
   const commitMsg = readFileSync(gitParams, "utf-8").trim();
 
   if (!commitRE.test(commitMsg) && !mergeRE.test(commitMsg)) {
-    consola.error(`invalid commit message: "${commitMsg}".
+    const example = isMonorepo() ? monorepoExample : baseExample;
+    consola.error(`提交代码的格式不正确: "${commitMsg}".
 
-Proper commit message format is required for automated changelog generation.
-
-Examples: 
-
-- fix(Button): incorrect style
-- feat(Button): incorrect style
-- docs(Button): fix typo
-
-Allowed Types:
+您需要按照下面的格式进行提交您本地的代码.
+${example}
+支持提交的类型:
 
 - fix
 - feat
